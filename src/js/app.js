6a0163a74a7d6bc05240687d89387b6d57c9d462
Angel Kirkov <angelnkirkov@gmail.com>
2021-09-17 17:05:51 +0300

    edit2

--- src/js/app.js
+++ src/js/app.js
@@ -6,4 +6,6 @@ window.addEventListener("DOMContentLoaded", () => {
   const numbersArray = [1, 3, 4, 5, 6, 7];
   const isEven = (n) => n % 2 === 0;
   const filteredArray = numbersArray.filter(isEven);
+  var li = document.createElement("li");
+  ul.appendChild(li);
 });
