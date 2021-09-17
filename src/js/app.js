f42b74d70df1c4a1c8ac32e4ae2bcc6f92a2419f
Angel Kirkov <angelnkirkov@gmail.com>
2021-09-17 16:22:47 +0300

    edit

--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,5 @@ window.addEventListener("DOMContentLoaded", () => {
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
   const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+  const filteredArray = numbersArray.filter(isEven);
 });
